b84c77022da04fd80f3b9aa2257012c42d13e4ff
Stas Borovikov <b-size3@ya.ru>
2026-02-21 00:11:23 +0300

    TAS-40: Переход на Java 25 + Spring Boot 4

--- auth-service/src/main/java/ru/taska/converter/JsonToJsonNodeConverter.java
+++ auth-service/src/main/java/ru/taska/converter/JsonToJsonNodeConverter.java
@@ -1,12 +1,11 @@
 package ru.taska.converter;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 import io.r2dbc.postgresql.codec.Json;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
-import org.springframework.lang.NonNull;
 
 @ReadingConverter
 @RequiredArgsConstructor
@@ -15,7 +14,7 @@ public class JsonToJsonNodeConverter implements Converter<Json, JsonNode> {
     private final ObjectMapper objectMapper;
 
     @Override
-    public JsonNode convert(@NonNull Json source) {
+    public JsonNode convert(Json source) {
         try {
             return objectMapper.readTree(source.asString());
         } catch (Exception e) {
--- auth-service/src/main/java/ru/taska/domain/OutboxEvent.java
+++ auth-service/src/main/java/ru/taska/domain/OutboxEvent.java
@@ -1,13 +1,12 @@
 package ru.taska.domain;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import tools.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
