3324667aa3351ca778a2fc0d65cf60e7d1af89ce
EkaterinaKat <56173952+EkaterinaKat@users.noreply.github.com>
2026-06-22 21:04:23 +0500

    TAS-BE-008: Исправить генерацию issueKey (#56)

--- issue-service/src/main/java/ru/taska/transport/grpc/GrpcProjectServiceClient.java
+++ issue-service/src/main/java/ru/taska/transport/grpc/GrpcProjectServiceClient.java
@@ -8,6 +8,9 @@ import ru.taska.api.common.v1.Header;
 import ru.taska.api.project.v1.CheckProjectMemberRoleRequest;
 import ru.taska.api.project.v1.CheckProjectMemberRoleRequestBody;
 import ru.taska.api.project.v1.CheckProjectMemberRoleResponse;
+import ru.taska.api.project.v1.GetProjectRequest;
+import ru.taska.api.project.v1.GetProjectRequestBody;
+import ru.taska.api.project.v1.ProjectResponse;
 import ru.taska.api.project.v1.ReactorProjectServiceGrpc;
 
 import java.util.UUID;
@@ -25,7 +28,7 @@ public class GrpcProjectServiceClient {
             UUID projectId,
             UUID userId
     ) {
-        log.debug("[{}][{}] Calling checkProjectRole with: projectId={}, userId={}",
+        log.info("[{}][{}] Calling checkProjectRole with: projectId={}, userId={}",
                 requestId, nodeId, projectId, userId);
 
         var request = CheckProjectMemberRoleRequest.newBuilder()
@@ -44,6 +47,27 @@ public class GrpcProjectServiceClient {
                 .build();
 
         return projectServiceStub.checkProjectMemberRole(request);
+    }
+
+    public Mono<String> getProjectKey(String requestId, String nodeId, UUID projectId) {
+        log.info("[{}][{}] Calling getProject with: projectId={}", requestId, nodeId, projectId);
+
+        var request = GetProjectRequest.newBuilder()
+                .setHeader(
+                        Header.newBuilder()
+                                .setRequestId(requestId)
+                                .setNodeId(nodeId)
+                                .build()
+                )
+                .setBody(
+                        GetProjectRequestBody.newBuilder()
+                                .setProjectId(projectId.toString())
+                                .build()
+                )
+                .build();
+
+        return projectServiceStub.getProject(request)
+                .map(ProjectResponse::getProjectKey);
     }
 
 }
