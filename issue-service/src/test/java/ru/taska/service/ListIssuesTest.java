0b85cb0a000493e751acc375a1c25f9ff29ee104
EkaterinaKat <Katerina290896@mail.ru>
2026-07-11 01:17:34 +0500

    fix

--- issue-service/src/test/java/ru/taska/service/ListIssuesTest.java
+++ issue-service/src/test/java/ru/taska/service/ListIssuesTest.java
@@ -18,6 +18,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
 
 class ListIssuesTest extends IssueServiceImplTest {
 
