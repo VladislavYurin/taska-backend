47c720814f3332fe783565195962b4529850f7c0
Grigoriy <quracau@gmail.com>
2026-09-01 16:50:23 +0300

    TAS-131: api-gateway: REST endpoints для issue attachments
    # Conflicts:
    #	api-gateway/src/main/java/ru/taska/config/GrpcClientConfig.java
    #	api-gateway/src/main/resources/static/openapi.yml

--- api-gateway/src/main/java/ru/taska/config/GrpcClientConfig.java
+++ api-gateway/src/main/java/ru/taska/config/GrpcClientConfig.java
@@ -7,10 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.taska.api.admin.v1.ReactorAdminServiceGrpc;
 import ru.taska.api.auth.v1.ReactorAuthServiceGrpc;
-import ru.taska.api.issue.v1.ReactorIssueServiceGrpc;
-import ru.taska.api.notification.v1.ReactorNotificationServiceGrpc;
-import ru.taska.api.project.v1.ReactorProjectServiceGrpc;
+import ru.taska.api.issue.attachment.v1.ReactorIssueAttachmentServiceGrpc;
 import ru.taska.api.workflow.v1.ReactorWorkflowServiceGrpc;
+import ru.taska.api.notification.v1.ReactorNotificationServiceGrpc;
+import ru.taska.api.issue.v1.ReactorIssueServiceGrpc;
+import ru.taska.api.project.v1.ReactorProjectServiceGrpc;
 import ru.taska.config.props.GrpcClientProperties;
 
 @Configuration
@@ -72,6 +73,11 @@ public class GrpcClientConfig {
                 )
                 .usePlaintext()
                 .build();
+    }
+
+    @Bean
+    public ReactorIssueAttachmentServiceGrpc.ReactorIssueAttachmentServiceStub issueAttachmentServiceStub() {
+        return ReactorIssueAttachmentServiceGrpc.newReactorStub(issueManagedChannel());
     }
 
     @Bean
